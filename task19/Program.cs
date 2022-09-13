f3d2e65a3c8a757981cec29a1e5951868fb9fa1c
Olga Gladkaya <tarantasik@mail.ru>
2022-09-13 19:49:51 +0300

    added file

---
+++ task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter a five-digit number:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(CheckPolindrome(number));
+string CheckPolindrome(int number)
+{
+    int digitCommon = number / 1000;
+    int digitSecond = digitCommon % 10;
+    int digitFirst = digitCommon / 10;
+    int digitCommon1 = number % 100;
+    int digitFour = digitCommon1 / 10;
+    int digitFive = digitCommon1 % 10;
+    if (digitFirst == digitFive && digitSecond == digitFour)
+    {
+        return "The number is a palindrome";
+    }
+    else
+    {
+        return "The number is not a palindrome";
+    }
+}
+
