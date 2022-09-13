40dbae81ed3425bba6d7345e9a7ea8cab703135e
Olga Gladkaya <tarantasik@mail.ru>
2022-09-13 19:40:29 +0300

    added file

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task23
+Console.WriteLine("Enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+void GetTable(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"The cube of the number {count} = {Math.Pow(count, 3)}");
+        count++;
+    }
+}
+GetTable(number);
