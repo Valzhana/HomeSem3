fbfed7801854f70a6a14ce2ad7f82bc722019049
Olga Gladkaya <tarantasik@mail.ru>
2022-09-13 19:50:33 +0300

    added file

---
+++ task21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task21
+Console.WriteLine("Enter coordinates of the first point:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter coordinates of the second point:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetDistance(x1, y1, z1, x2, y2, z2));
+double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return distance;
+}
