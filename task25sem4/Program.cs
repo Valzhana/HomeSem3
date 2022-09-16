c9268c80f4e81164e1de7bdb7f511747b40f8f9e
Olga Gladkaya <tarantasik@mail.ru>
2022-09-16 18:41:31 +0300

    added file

---
+++ task25sem4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task25
+Console.WriteLine("Enter number A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+void ExponentiateNumber(int numberA, int numberB)
+{
+    int count = 0;
+    int result = 1;
+    while (count < numberB)
+    {
+        result = result*numberA;
+        count++;
+    }
+    Console.WriteLine($"NumberA to the power of numberB is: {result}");
+}
+ExponentiateNumber(numberA, numberB);
+
