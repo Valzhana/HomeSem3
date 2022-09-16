2b79e3f5c1ce63beb3d7e33b9c87148c7357ef83
Olga Gladkaya <tarantasik@mail.ru>
2022-09-16 19:01:31 +0300

    added file

---
+++ task27sem4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task27
+Console.WriteLine("Enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+void GetDigitSum(int number)
+{
+    int sum = 0;
+    number = number*10;
+    while (number > 0)
+    {
+        number = number / 10;
+        int result = number % 10;
+        sum = sum + result;
+    }
+    Console.WriteLine($"The sum of the digits in the number is: {sum}");
+}
+GetDigitSum(number);
+
