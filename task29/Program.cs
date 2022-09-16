797b86e89e7b271bc7863bdb8f8511436d5261a2
Olga Gladkaya <tarantasik@mail.ru>
2022-09-16 19:20:46 +0300

    added file

---
+++ task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task29
+int[] CreateArray()
+{
+    Random rnd = new Random();
+    int[] array = new int[8];
+    for(int index = 0; index < array.Length; index++)
+    {
+        array[index] = rnd.Next();
+    }
+    return array;
+}
+void PrintArray(int[]array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"{array[index]}, ");
+    }
+}
+int[] arr = CreateArray();
+PrintArray(arr);
