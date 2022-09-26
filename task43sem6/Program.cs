65f5136f50bfd39276b1c4f6b13152c125ab5381
Olga Gladkaya <tarantasik@mail.ru>
2022-09-26 11:54:20 +0300

    added file

---
+++ task43sem6/Program.cs
@@ -0,0 +1,30 @@
+﻿// task43
+Console.WriteLine("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+void FindIntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x;
+    double y;
+    if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine("The lines coincide");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine("The lines are parallel");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k2 * x + b2;
+        Console.WriteLine($"The intersection point is:({x};{y})");
+    }
+}
+FindIntersectionPoint(b1, k1, b2, k2);
+
