e56749eca2efd07442d005ac437fad0ccc8bcb13
Olga Gladkaya <tarantasik@mail.ru>
2022-10-01 22:12:51 +0300

    added file

---
+++ task47sem7/Program.cs
@@ -0,0 +1,21 @@
+﻿// task47
+Console.Write("Enter the number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] CreateArray()
+{
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next());
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+CreateArray();
+
