e6cbe9c4d4ecae1de82f6ad8aa9e4cd0fa18bb42
Olga Gladkaya <tarantasik@mail.ru>
2022-10-04 18:47:13 +0300

    added file

---
+++ task66sem9/Program.cs
@@ -0,0 +1,26 @@
+﻿// task66
+Console.WriteLine("Enter the first number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int FindSumOfElements(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        if (firstNumber == secondNumber)
+        {
+            return firstNumber;
+        }
+        return firstNumber + FindSumOfElements(firstNumber - 1, secondNumber); 
+    }
+    else
+    {
+        if (firstNumber == secondNumber)
+        {
+            return firstNumber;
+        }
+        return firstNumber + FindSumOfElements(firstNumber + 1, secondNumber);
+    }
+}
+Console.WriteLine($"The sum of the elements is: {(FindSumOfElements(firstNumber, secondNumber))}");
+
