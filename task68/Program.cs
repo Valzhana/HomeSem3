c2058a829e35019212dd3eca392d09f5575baadf
Olga Gladkaya <tarantasik@mail.ru>
2022-10-04 18:51:15 +0300

    added file

---
+++ task68/Program.cs
@@ -0,0 +1,21 @@
+﻿// task68
+Console.WriteLine("Enter the first non-negative number:");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second non-negative number:");
+int numN = Convert.ToInt32(Console.ReadLine());
+int CalcAckermannFunction(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    else if (numN == 0 && numM > 0)
+    {
+        return CalcAckermannFunction(numM - 1, 1);
+    }
+    else
+    {
+        return CalcAckermannFunction(numM - 1, CalcAckermannFunction(numM, numN - 1));
+    }
+}
+Console.WriteLine($"Calculation result is: {CalcAckermannFunction(numM, numN)}");
