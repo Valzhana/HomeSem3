d9e155955699ecf6e81e9255dab010efc959e76b
Olga Gladkaya <tarantasik@mail.ru>
2022-10-04 18:42:41 +0300

    added file

---
+++ task64sem9/Program.cs
@@ -0,0 +1,12 @@
+﻿// task64
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumber(number, 1));
+string PrintNumber(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return(start +"," + PrintNumber(start -1, end));
+}
